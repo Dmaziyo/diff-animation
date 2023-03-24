57450939ca80895edfbb3e8add528565803ccc83
D城兔Maziyo <247948681@qq.com>
2023-03-24 16:43:15 +0800

    feat: handle c2.len is less than c1.len

--- diff.js
+++ diff.js
@@ -1,17 +1,70 @@
-const numberOfElements = 20
 let numbers = []
+let numbers2 = []
 let container1
 let container2
 let delayContainer
-function init() {
+let swapBtn = document.getElementById('swap')
+let resetBtn = document.getElementById('reset')
+
+swapBtn.addEventListener('click', () => {
+  patchKeyedChildren(container1.children, container2.children, container1)
+})
+resetBtn.addEventListener('click', () => {
+  init(type)
+})
+document.addEventListener('DOMContentLoaded', () => {
+  init(type)
+  // renderNumbers(container1, numbers)
+  // numbers.sort((a, b) => (Math.random() > 0.5 ? 1 : -1))
+  // renderNumbers(container2, numbers)
+  // patchKeyedChildren(container1.children, container2.children, container1)
+})
+
+/**
+    type:0 default   //生成相同但顺序不同的  
+    type:1           //c2元素比c1少  
+    type:2           //c2元素比c1多  
+ */
+function init(type = 0) {
+  let numberOfElements = 20
+
+  numbers.length = 0
+  numbers2.length = 0
   container1 = document.getElementById('numbers-old')
   container2 = document.getElementById('numbers-new')
-  for (let i = 1; i <= numberOfElements; i++) {
-    numbers.push(i)
+  switch (type) {
+    case 0:
+      for (let i = 1; i <= numberOfElements; i++) {
+        numbers.push(i)
+      }
+      renderNumbers(container1, numbers)
+      numbers.sort(() => (Math.random() > 0.5 ? 1 : -1))
+      renderNumbers(container2, numbers)
+      break
+    case 1:
+      debugger
+
+      for (let i = 1; i <= numberOfElements; i++) {
+        numbers.push(i)
+      }
+      numberOfElements = getRandom(1, getRandom(2, 19))
+      let set = new Set()
+      while (!(set.size === numberOfElements)) {
+        set.add(getRandom(1, 19))
+      }
+      numbers2 = Array.from(set)
+      renderNumbers(container1, numbers)
+      numbers2.sort((a, b) => (Math.random() > 0.5 ? 1 : -1))
+      renderNumbers(container2, numbers2)
+      break
+    case 2:
+      //  todo c2.length longer than c1.length
+      break
   }
 }
 
 function renderNumbers(container, numbers) {
+  container.innerHTML = ''
   for (const number of numbers) {
     let numberRepresentationContainer = document.createElement('div')
     numberRepresentationContainer.classList.add('number')
@@ -21,12 +74,11 @@ function renderNumbers(container, numbers) {
 }
 
 async function insertBefore(elements, target, anchor, parent) {
-  target.classList.add('inspected-number')
   await sleep(500)
   elements = Array.from(elements)
   const targetIndex = elements.indexOf(target)
 
-  const animationLength = 100
+  const animationLength = 2000
   let anchorIndex = elements.indexOf(anchor)
   anchorIndex = anchorIndex === -1 ? elements.length : anchorIndex
   let columnWidth = elements[0].clientWidth + 20
@@ -34,7 +86,6 @@ async function insertBefore(elements, target, anchor, parent) {
   let condition = targetIndex < anchorIndex ? -1 : 1
   let length = condition === -1 ? anchorIndex : targetIndex
   for (let i = condition === -1 ? targetIndex + 1 : anchorIndex; i < length; i++) {
-    debugger
     elements[i].setAttribute(
       'style',
       `transform: translate(${columnWidth * condition}px, 0px);
@@ -54,12 +105,26 @@ async function insertBefore(elements, target, anchor, parent) {
   }
 
   parent.insertBefore(target, anchor)
+  // remove the animation
   elements[targetIndex].removeAttribute('style')
   await sleep(500)
-  elements[targetIndex].classList.remove('inspected-number')
-  elements[anchorIndex] && elements[anchorIndex].classList.remove('anchor-number')
 }
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
+async function tagAndRemove(node) {
+  node.classList.add('to-be-removed')
+  debugger
+  console.log(node)
+  await sleep(transiLenth)
+  node.parentNode.removeChild(node)
+  node.classList.remove('to-be-removed')
+  await sleep(transiLenth)
+  return
 }
